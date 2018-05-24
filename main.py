ed182ad99b4af47a2d4d07984dc7be2779ad5ef1
rohrz4nge <maximilian.lohmann.hh@gmail.com>
2018-05-24 18:57:43 +0200

    Add files via upload
    Initial upload

---
+++ main.py
@@ -0,0 +1,55 @@
+from pynput.keyboard import Key, Controller
+from time import sleep
+
+keyboard = Controller()
+
+"""
+function writing a character to the stdin
+possible parameters:
+    character: the character to be printed, either a string with length 1, a member of Key or KeyCode"""
+def write_char_to_stdin(character=None):
+    if character:
+        keyboard.press(character)
+        keyboard.release(character)        
+
+"""
+function writing a string and a newline at the end to the stdin
+possible parameters:
+    text: a string of text
+    typing_timeout: the timeout between each keystroke in seconds """
+def write_string_to_stdin(text=None, typing_timeout=0.05):
+    for ch in text:
+        write_char_to_stdin(ch)
+        sleep(typing_timeout)
+    # writing the new line
+    write_char_to_stdin(Key.enter)
+
+"""
+function writing multiple lines of text into the stdin, either from a supplied text or a file
+possible arguments:
+    text: a string of text
+    path_to_file: a string as system path
+    starting_pause: the timeout on initial start of the program in seconds
+    pause_between_lines: the timeout between each written line in the input in seconds
+    typing_timeout: the timeout between each keystroke in seconds """
+def write_lines_to_stdin(text=None, path_to_file=None, starting_pause=5, pause_between_lines=2, typing_timeout=0.05):
+    # splitting the text if text is given
+    if text:
+        all_lines = text.splitlines()
+    # opening the text file if a file is given
+    elif path_to_file:
+        with open(path_to_file, encoding='utf-8') as f:
+            all_lines = f.read().splitlines()
+    # if the required arguments are missing the error code is returned
+    else:
+        return -1
+
+    sleep(starting_pause)
+    # writing each line
+    for line in all_lines:
+        write_string_to_stdin(line, typing_timeout)
+        sleep(pause_between_lines)
+    return 0
+
+if __name__ == "__main__":
+    write_lines_to_stdin(None, 'C:/Users/Maxi/Desktop/eloah.txt')
