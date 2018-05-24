7f01643dd4fb5f5515ec1d3a24e44e73880e6967
rohrz4nge <maximilian.lohmann.hh@gmail.com>
2018-05-25 00:55:24 +0200

    Function name simplifications

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ keyboard = Controller()
 function writing a character to the stdin
 possible parameters:
     character: the character to be printed, either a string with length 1, a member of Key or KeyCode"""
-def write_char_to_stdin(character=None):
+def write_char(character=None):
     if character:
         keyboard.press(character)
         keyboard.release(character)        
@@ -17,12 +17,12 @@ function writing a string and a newline at the end to the stdin
 possible parameters:
     text: a string of text
     typing_timeout: the timeout between each keystroke in seconds """
-def write_string_to_stdin(text=None, typing_timeout=0.05):
+def write_string(text=None, typing_timeout=0.05):
     for ch in text:
-        write_char_to_stdin(ch)
+        write_char(ch)
         sleep(typing_timeout)
     # writing the new line
-    write_char_to_stdin(Key.enter)
+    write_char(Key.enter)
 
 """
 function writing multiple lines of text into the stdin, either from a supplied text or a file
@@ -32,15 +32,15 @@ possible arguments:
     starting_pause: the timeout on initial start of the program in seconds
     pause_between_lines: the timeout between each written line in the input in seconds
     typing_timeout: the timeout between each keystroke in seconds """
-def write_lines_to_stdin(text=None, path_to_file=None, starting_pause=5, pause_between_lines=2, typing_timeout=0.05):
+def write_lines(text=None, path_to_file=None, starting_pause=5, pause_between_lines=2, typing_timeout=0.05):
     # splitting the text if text is given
     if text:
-        all_lines = text.splitlines()
+        text_lines = text.splitlines()
     # opening the text file if a path is given
     elif path_to_file:
         try:
             with open(path_to_file, encoding='utf-8') as f:
-                all_lines = f.read().splitlines()
+                text_lines = f.read().splitlines()
         except FileNotFoundError:
             raise FileNotFoundError('Wrong file or path')
     
@@ -50,9 +50,9 @@ def write_lines_to_stdin(text=None, path_to_file=None, starting_pause=5, pause_b
     
     sleep(starting_pause)
     # writing each line
-    for line in all_lines:
-        write_string_to_stdin(line, typing_timeout)
+    for line in text_lines:
+        write_string(line, typing_timeout)
         sleep(pause_between_lines)
 
 if __name__ == "__main__":
-    write_lines_to_stdin(path_to_file='C:/file.txt')
+    write_lines(path_to_file='C:/file.txt')
