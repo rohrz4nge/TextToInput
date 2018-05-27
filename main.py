4c479dfcdce9e64a4c6aad14f9c8fe50162bc05c
rohrz4nge <maximilian.lohmann.hh@gmail.com>
2018-05-27 18:46:00 +0200

    Improved readablility

--- main.py
+++ main.py
@@ -1,58 +1,93 @@
 from pynput.keyboard import Key, Controller
 from time import sleep
 
+
 keyboard = Controller()
 
-"""
-function writing a character to the stdin
-possible parameters:
-    character: the character to be printed, either a string with length 1, a member of Key or KeyCode"""
+
 def write_char(character=None):
+    """Write a character on the keyboard
+    
+    Keyword arguments:
+    character -- the character to be printed, either a string with length 1, a member of Key or KeyCode
+    """
+    
     if character:
         keyboard.press(character)
         keyboard.release(character)        
 
-"""
-function writing a string and a newline at the end to the stdin
-possible parameters:
-    text: a string of text
-    typing_timeout: the timeout between each keystroke in seconds """
+
 def write_string(text=None, typing_timeout=0.05):
+    """Write a string and a newline at the end on the keyboard
+    
+    Keyword arguments:
+    text -- a string of text
+    typing_timeout -- the timeout between each keystroke in seconds
+    """
+    
     for ch in text:
         write_char(ch)
         sleep(typing_timeout)
     # writing the new line
     write_char(Key.enter)
 
-"""
-function writing multiple lines of text into the stdin, either from a supplied text or a file
-possible arguments:
-    text: a string of text
-    path_to_file: the path to a text file, as string
-    starting_pause: the timeout on initial start of the program in seconds
-    pause_between_lines: the timeout between each written line in the input in seconds
-    typing_timeout: the timeout between each keystroke in seconds """
-def write_lines(text=None, path_to_file=None, starting_pause=5, pause_between_lines=2, typing_timeout=0.05):
-    # splitting the text if text is given
-    if text:
-        text_lines = text.splitlines()
-    # opening the text file if a path is given
-    elif path_to_file:
-        try:
-            with open(path_to_file, encoding='utf-8') as f:
-                text_lines = f.read().splitlines()
-        except FileNotFoundError:
-            raise FileNotFoundError('Wrong file or path')
+
+def write_lines(text=None, starting_pause=5, pause_between_lines=2, typing_timeout=0.05):
+    """Write multiple lines of text on the keyboard
     
-    # if the required arguments are missing the error code is returned
-    else:
-        raise ValueError('The supplied parameters have to include either a string or the path to a textfile')
+    Keyword arguments:
+    text -- a list of strings of text, each representing a line
+    starting_pause -- the timeout on initial start of the program in seconds
+    pause_between_lines -- the timeout between each written line in the input in seconds
+    typing_timeout -- the timeout between each keystroke in seconds
+    """
     
     sleep(starting_pause)
     # writing each line
-    for line in text_lines:
+    for line in text:
         write_string(line, typing_timeout)
         sleep(pause_between_lines)
+        
+def write_text(text=None, path=None, starting_pause=5, pause_between_lines=2, typing_timeout=0.05):
+    """ Write multiple lines of text on the keyboard, either from a supplied text or a file
+    
+    Keyword arguments:
+    text -- a string of text
+    path -- the path to a text file, as string
+    starting_pause -- the timeout on initial start of the program in seconds
+    pause_between_lines -- the timeout between each written line in the input in seconds
+    typing_timeout -- the timeout between each keystroke in seconds
+    """
+    # splitting the text if text is given
+    if text:
+        text_lines = text.splitlines()
+    # opening the text file
+    elif path:
+        text_lines = read_file(path)
+        # if the reading operation wasn't successful the function is stopped
+        if text_lines is None:
+            return None
+    # if the required arguments are missing the error code is returned
+    else:
+        raise ValueError('The supplied parameters have to include either a string or the path to a textfile')
+    # writing the lines of text
+    write_lines(text_lines, starting_pause, pause_between_lines, typing_timeout)
+
+
+def read_file(path):
+    """Read a list of text lines, as string, from the given file path
+    
+    Keyword Arguments:
+    path -- the path to a text file, as string
+    """
+    
+    try:
+        with open(path, encoding='utf-8') as f:
+            text_lines = f.read().splitlines()
+    except FileNotFoundError:
+        raise
+    return text_lines
+    
 
 if __name__ == "__main__":
-    write_lines(path_to_file='C:/file.txt')
+    write_text(text='Lorem ipsum dolor sit amet\nconsetetur sadipscing elitr')
